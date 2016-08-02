1a9444f92dade25253d825b7b9e754543028155d
Roman Babyuk <rbabyuk@mirantis.com>
2016-08-02 11:12:21 +0300

    moved variables to params.pp file; added validation if variables are string type

--- deployment_scripts/puppet/modules/lma_infrastructure_alerting_sfdc/manifests/init.pp
+++ deployment_scripts/puppet/modules/lma_infrastructure_alerting_sfdc/manifests/init.pp
@@ -22,22 +22,26 @@ class lma_infrastructure_alerting_sfdc (
   $password,
   $env,
   $organization_id,
-  $plugin_dir                 =  '/usr/lib/nagios/plugins/sfdc/',
-  $plugin_lib_file            =  '/usr/lib/nagios/plugins/sfdc/salesforce.py',
-  $plugin_file                =  '/usr/lib/nagios/plugins/sfdc/sfdc_nagios.py',
-  $plugin_config_file         =  '/usr/lib/nagios/plugins/sfdc/sfdc_nagios.yaml',
-  $plugin_nagios_config_file  =  '/etc/nagios3/conf.d/sfdc_commands.cfg',
-  $plugin_log_file            =  '/var/log/nagios_to_sfdc.log',
-  $nagios_service_name        =  'nagios3',
-  $nagios_contacts_file       =  '/etc/nagios3/conf.d/sfdc_contacts.cfg',
-  $nagios_commands_file       =  '/etc/nagios3/conf.d/sfdc_commands.cfg',
-  $logrotate_config           =  '/etc/logrotate.d/sfdc_nagios'
+  $plugin_dir                 =  $::lma_infrastructure_alerting_sfdc::params::plugin_dir,
+  $plugin_lib_file            =  $::lma_infrastructure_alerting_sfdc::params::plugin_lib_file,
+  $plugin_file                =  $::lma_infrastructure_alerting_sfdc::params::plugin_file,
+  $plugin_config_file         =  $::lma_infrastructure_alerting_sfdc::params::plugin_config_file,
+  $plugin_nagios_config_file  =  $::lma_infrastructure_alerting_sfdc::params::plugin_nagios_config_file,
+  $plugin_log_file            =  $::lma_infrastructure_alerting_sfdc::params::plugin_log_file,
+  $nagios_service_name        =  $::lma_infrastructure_alerting_sfdc::params::nagios_service_name,
+  $nagios_contacts_file       =  $::lma_infrastructure_alerting_sfdc::params::nagios_contacts_file,
+  $nagios_commands_file       =  $::lma_infrastructure_alerting_sfdc::params::nagios_commands_file,
+  $logrotate_config           =  $::lma_infrastructure_alerting_sfdc::params::logrotate_config,
 
-)  {
+) inherits lma_infrastructure_alerting_sfdc::params {
 
   notify {'lma_infrastructure_alerting_sfdc start': }
 
-  service { $nagios_service_name:
+  validate_string($auth_url, $client_id, $client_secret, $username, $password, $env, $organization_id, $plugin_dir,
+                  $plugin_lib_file, $plugin_file, $plugin_config_file, $plugin_nagios_config_file, $plugin_log_file,
+                  $nagios_service_name, $nagios_contacts_file, $nagios_commands_file, $logrotate_config)
+
+  service { "${nagios_service_name}":
       ensure => running,
       enable => true,
     }
